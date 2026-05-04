0b306dfb7fd61df47fb51d936162bce821abf665
skope-kitchens <sathya@skopekitchens.com>
2026-05-04 15:54:49 +0530

    simplify cors config

--- backend/index.js
+++ backend/index.js
@@ -70,15 +70,7 @@ io.on("connection", (socket) => {
 app.set("io", io);
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    } else {
-      return callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: allowedOrigins,
   credentials: true
 }));
 
